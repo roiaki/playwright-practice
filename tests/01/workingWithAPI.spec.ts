579451e7e4fdf98c75713c258076213437f2de8f
roiaki <roitoitoitoi@yahoo.co.jp>
2025-04-11 00:14:34 +0900

    fix json

--- tests/01/workingWithAPI.spec.ts
+++ tests/01/workingWithAPI.spec.ts
@@ -1,20 +1,20 @@
 import { test, expect } from '@playwright/test';
-import { tags } from '../../test-data/tags.json';
+import tags from '../../test-data/tags.json';
 
 
 test.beforeEach(async ({ page }) => {
   await page.route('https://conduit-api.bondaracademy.com/api/tags', async route => {
-    const tags = {
-      "tags": [
-        "automation",
-        "playwright",
-      ]
-    }
+    // const tags = {
+    //   "tags": [
+    //     "automation",
+    //     "playwright",
+    //   ]
+    // }
     await route.fulfill({
       body: JSON.stringify(tags)
     })
   })
-
+  console.log(tags);
   await page.waitForTimeout(1000);
   await page.goto('https://conduit.bondaracademy.com/');
 });
